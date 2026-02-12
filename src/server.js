d61c62da825bafb10fb8ccb03e26302c2afc64ce
aarvionservices-ship-it <aarvionservices@gmail.com>
2026-02-12 23:16:43 +0530

    Update server.js
    API added

--- src/server.js
+++ src/server.js
@@ -17,7 +17,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://frontend-eight-ruddy-22.vercel.app',
+    'https://aarvionservices.com',
     process.env.CLIENT_URL
 ].filter(Boolean); // Remove undefined values
 
